ac03ac3f06bef4e907c042e48f9bf6ebd34b9bc3
Bernardo Santiago Marin <bernardosantiago44@gmail.com>
2023-11-02 15:35:32 -0600

    Created Shared Data Model.

---
+++ SalesTracker/SalesTracker/Model/SalesModel.swift
@@ -0,0 +1,12 @@
+//
+//  Shared.swift
+//  SalesTracker
+//
+//  Created by Bernardo Santiago Marin on 02/11/23.
+//
+
+import Foundation
+
+final class SalesModel: ObservableObject {
+    @Published var Products = [Product]()
+}
