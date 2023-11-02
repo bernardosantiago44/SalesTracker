516211500f70f694966e600a2837f4b79b38e2fa
Bernardo Santiago Marin <bernardosantiago44@gmail.com>
2023-11-02 13:52:31 -0600

    Update Product class.

--- SalesTracker/SalesTracker/Model/Product.swift
+++ SalesTracker/SalesTracker/Model/Product.swift
@@ -6,3 +6,60 @@
 //
 
 import Foundation
+
+// Creating a class to save data through
+// Swift Data
+//
+class Product: Identifiable {
+    var id: String
+    
+    var name: String
+    
+    var icon: String
+    
+    var color: String
+    
+    private var price: Float
+    
+    init() {
+        self.id = UUID().uuidString
+        self.name = "Product name"
+        self.icon = "cup.and.saucer"
+        self.color = "green"
+        self.price = 0
+    }
+    
+    init(name: String, icon: String, color: String, price: Float) {
+        self.id = UUID().uuidString
+        self.name = name
+        self.icon = icon
+        self.color = color
+        self.price = price
+    }
+    
+    func updateIcon() {
+        
+    }
+    
+    func updateColor(newColor: String) {
+        
+    }
+    
+    func updatePrice(newPrice: Float) {
+        
+    }
+    
+    func getPrice() -> Float {
+        return self.price
+    }
+    
+    func updatePrice(to newPrice: Float) -> Float {
+        let oldPrice = self.price
+        self.price = newPrice
+        // Methods to update price information
+        // in Data Model.
+        return oldPrice
+    }
+    
+    
+}
