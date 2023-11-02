b237c53078cff39111ce22cc2fd3db9a7cafa2bf
Bernardo Santiago <bernardosantiago44@gmail.com>
2023-11-02 17:39:59 -0600

    Updated ColorPicker and finished New Product Form

--- SalesTracker/SalesTracker/View/ProductManagement/NewProductView.swift
+++ SalesTracker/SalesTracker/View/ProductManagement/NewProductView.swift
@@ -9,11 +9,21 @@ import SwiftUI
 
 struct NewProductView: View {
     @ObservedObject var salesModel: SalesModel
-    @State private var product = Product()
+    @State private var productName = "Product name"
+    @State private var categoryColor: Color = .green
+    @State private var productPrice: Float = 0
+    let currencyCode = Locale.current.currency?.identifier ?? "usd"
+    
     var body: some View {
         Form {
             Section {
-                TextField("productname", text: $product.name)
+                TextField("productname", text: $productName)
+                TextField(value: $productPrice, format: .currency(code: self.currencyCode)) {
+                    Text("currency")
+                }
+            }
+            Section {
+                ColorPicker(selectedColor: self.$categoryColor)
             }
         }
     }
