c03c7761cc0dade59cfea221fcfdd5664c6502c1
Bernardo Santiago Marin <bernardosantiago44@gmail.com>
2023-11-02 12:50:00 -0600

    Initial commit

---
+++ SalesTracker/SalesTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SalesTracker
+//
+//  Created by Bernardo Santiago Marin on 02/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
