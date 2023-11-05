6ff8cc9290cc1c61c8b3b4e77d463cc9f56af1c4
Bernardo Santiago <bernardosantiago44@gmail.com>
2023-11-05 12:22:58 -0600

    Created Unit Tests.

--- SalesTracker/SalesTracker/Model/SalesModel.swift
+++ SalesTracker/SalesTracker/Model/SalesModel.swift
@@ -10,4 +10,12 @@ import Foundation
 final class SalesModel: ObservableObject {
     @Published var Products = [Product]()
     @Published var sampleProducts: [Product] = Product.SampleProducts
+    
+    func addProductToCatalog(_ product: Product) -> ActionReponse {
+        // Check if product contains all essential elements. 
+        guard product.isValid() else { return .Unsuccessful }
+        
+        self.Products.append(product)
+        return .Successful
+    }
 }
