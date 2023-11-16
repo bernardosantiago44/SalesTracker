c240086c4705abc9f0fd5d869c3a61905cab1d08
Bernardo Santiago <bernardosantiago44@gmail.com>
2023-11-15 18:30:12 -0600

    Added data validation for email and password fields.

---
+++ SalesTracker/SalesTracker/View/Authentication/AuthenticationFields.swift
@@ -0,0 +1,16 @@
+//
+//  AuthenticationFields.swift
+//  SalesTracker
+//
+//  Created by Bernardo Santiago Marin on 15/11/23.
+//
+
+import Foundation
+
+/// Used in the Login View to move between
+/// fields with keyboard shortcuts.
+///
+enum AuthField {
+    case email
+    case password
+}
--- SalesTracker/SalesTracker/View/Authentication/LoginView.swift
+++ SalesTracker/SalesTracker/View/Authentication/LoginView.swift
@@ -12,28 +12,25 @@ struct LoginView: View {
     @State private var email = ""
     @State private var password = ""
     @ObservedObject var authHandler = AuthenticationHandler()
-    @State private var authenticationStatus: AuthenticationStatus?
+    @FocusState var focusField: AuthField?
+    
+    var emailValidated: Bool {
+        let regex = try! NSRegularExpression(pattern: "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$")
+        let range = NSRange(location: 0, length: email.utf16.count)
+        return regex.firstMatch(in: email, range: range) != nil
+    }
+    
+    var passwordValidated: Bool {
+        return password.count >= 6
+    }
     
     var body: some View {
         VStack {
             // TODO: Add the logo of the App here when designed.
             // MARK: email and password fields
-            Text(Auth.auth().currentUser?.email ?? "")
             Group {
-                TextField("email", text: self.$email)
-                    .textInputAutocapitalization(.never)
-                    .autocorrectionDisabled()
-                    .keyboardType(.emailAddress)
-                    .textContentType(.username)
-                    .submitLabel(.next)
-                
-                SecureField("password", text: self.$password)
-                    .autocorrectionDisabled()
-                    .keyboardType(.asciiCapable)
-                    .textContentType(.password)
-                    .submitLabel(.done)
-                
-                
+                EmailTextField
+                PasswordField
             }
             .textFieldStyle(RoundedGroupTextFieldStyle())
             .padding(.horizontal)
@@ -54,6 +51,7 @@ struct LoginView: View {
                     }
                     .padding(.vertical, 20)
                 }
+                .disabled(!emailValidated || !passwordValidated)
             }
             .padding(.horizontal)
         }
@@ -84,6 +82,39 @@ struct LoginView: View {
             Text("login")
         }
         .buttonStyle(.borderedProminent)
+    }
+    
+    var EmailTextField: some View {
+        HStack {
+            TextField("email", text: self.$email)
+                .textInputAutocapitalization(.never)
+                .autocorrectionDisabled()
+                .keyboardType(.emailAddress)
+                .textContentType(.username)
+                .submitLabel(.next)
+                .focused(self.$focusField, equals: .email)
+                .onSubmit {
+                    self.focusField = .password
+                }
+            Image(systemName: emailValidated ? "checkmark.circle.fill" : "xmark.circle.fill")
+                .foregroundStyle(emailValidated ? .green : .red)
+        }
+    }
+    
+    var PasswordField: some View {
+        HStack {
+            SecureField("password", text: self.$password)
+                .autocorrectionDisabled()
+                .keyboardType(.asciiCapable)
+                .textContentType(.password)
+                .submitLabel(.done)
+                .focused(self.$focusField, equals: .password)
+                .onSubmit {
+                    self.focusField = nil
+                }
+            Image(systemName: passwordValidated ? "checkmark.circle.fill" : "xmark.circle.fill")
+                .foregroundStyle(passwordValidated ? .green : .red)
+        }
     }
 }
 
