2899ee5e0af4d92ba940ea587d900ff585e5a1e8
Bernardo Santiago <bernardosantiago44@gmail.com>
2023-11-19 23:05:51 -0600

    Created test class for email validation.

--- SalesTracker/SalesTrackerTests/SalesTrackerTests.swift
+++ SalesTracker/SalesTrackerTests/SalesTrackerTests.swift
@@ -19,7 +19,7 @@ final class SalesTrackerTests: XCTestCase {
     }
 
     func testProductPriceRejectsNegativeValues() throws {
-        let MockProduct = Product(name: "Test", category: "testing", color: .black, price: 10)
+        var MockProduct = Product(name: "Test", category: "testing", color: .black, price: 10)
         
         XCTAssertTrue(MockProduct.isValid())
         XCTAssertEqual(MockProduct.getPrice(), 10)
